d0ece0747abb76702b54bd651698265054003809
Xander Frangos <33106561+xanderfrangos@users.noreply.github.com>
2019-12-01 20:58:16 -0500

    Reimplemented drag-and-drop

--- src/app.main.js
+++ src/app.main.js
@@ -6,3 +6,21 @@ const files = []
 window.files = files
 
 ReactDOM.render(<App />, document.getElementById("crushee"));
+
+document.body.addEventListener("dragover", function (event) {
+    // prevent default to allow drop
+    console.log("drag", event, event.dataTransfer.files)
+    event.dataTransfer.dropEffect = 'copy';
+    event.preventDefault();
+}, false);
+
+document.body.addEventListener("drop", (event) => {
+    event.preventDefault();
+    console.log("dropFiles", event.dataTransfer.files, event.dataTransfer.items)
+    let files = []
+    for(let file of event.dataTransfer.files) {
+        console.log(file)
+        files.push(file.path)
+    }
+    window.addFiles(files)
+}, false);
