94f3780adebe05393e2c251870d264b3bb7a69e8
Xander Frangos <33106561+xanderfrangos@users.noreply.github.com>
2019-12-02 00:49:12 -0500

    Update app.main.js

--- src/app.main.js
+++ src/app.main.js
@@ -9,17 +9,15 @@ ReactDOM.render(<App />, document.getElementById("crushee"));
 
 document.body.addEventListener("dragover", function (event) {
     // prevent default to allow drop
-    console.log("drag", event, event.dataTransfer.files)
+
     event.dataTransfer.dropEffect = 'copy';
     event.preventDefault();
 }, false);
 
 document.body.addEventListener("drop", (event) => {
     event.preventDefault();
-    console.log("dropFiles", event.dataTransfer.files, event.dataTransfer.items)
     let files = []
-    for(let file of event.dataTransfer.files) {
-        console.log(file)
+    for (let file of event.dataTransfer.files) {
         files.push(file.path)
     }
     window.addFiles(files)
