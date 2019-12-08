84527bb4bd4eab589671144f99cbc3dec605f6d6
Xander Frangos <33106561+xanderfrangos@users.noreply.github.com>
2019-12-07 21:36:55 -0500

    Update InputToggle.jsx

--- src/components/input/InputToggle.jsx
+++ src/components/input/InputToggle.jsx
@@ -1,1 +1,21 @@
 // <input onChange={this.startupChanged} checked={window.settings.openAtLogin || false} data-checked={window.settings.openAtLogin || false} type="checkbox" id="theme" />
+
+
+
+
+
+
+<div className="row">
+    <div className="col">
+        <label>Crop</label>
+        <div className="sublabel">Width and height required</div>
+    </div>
+    <div className="col">
+        <div className="input--toggle" data-for="resizeCrop" tabIndex="0" data-linked="resize.crop" onClick={ (e) => {
+            
+        }}>
+            <div></div>
+            <input type="hidden" name="resize.crop" id="resizeCrop" data-linked="resize.crop" />
+        </div>
+    </div>
+</div>
