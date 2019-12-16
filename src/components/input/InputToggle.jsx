7784265dd17749ea386fdbcf1ae4e23de127e83f
Xander Frangos <33106561+xanderfrangos@users.noreply.github.com>
2019-12-15 20:21:55 -0500

    Update InputToggle.jsx

--- src/components/input/InputToggle.jsx
+++ src/components/input/InputToggle.jsx
@@ -40,7 +40,7 @@ export default class InputToggle extends PureComponent {
                 </div>
                 <div className="col">
                     <div className="input--toggle" data-value={window.GlobalSettings.Quality[keys[0]][keys[1]] || false} onClick={(e) => {
-                        window.GlobalSettings.Quality[keys[0]][keys[1]] = (window.GlobalSettings.Quality[keys[0]][keys[1]] === false ? true : false)
+                        window.GlobalSettings.Quality[keys[0]][keys[1]] = (window.GlobalSettings.Quality[keys[0]][keys[1]] === true ? false : true)
                         this.setState({ value: window.GlobalSettings.Quality[keys[0]][keys[1]] })
                     }}>
                         <div></div>
