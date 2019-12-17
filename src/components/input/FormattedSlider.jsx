ada251fc7eee10ff4d9c1c756bdc1dd9f0f19ce8
Xander Frangos <33106561+xanderfrangos@users.noreply.github.com>
2019-12-16 19:27:17 -0500

    Disable scrolling sliders

--- src/components/input/FormattedSlider.jsx
+++ src/components/input/FormattedSlider.jsx
@@ -8,7 +8,7 @@ export const FormattedSlider = (props) => {
         <div className="row">
             <div className="col flex-1">
                 <label>{props.name}</label>
-                <InputSlider level={window.GlobalSettings.Quality[keys[0]][keys[1]] || 0} onChange={(newVal) => { window.GlobalSettings.Quality[keys[0]][keys[1]] = newVal }} min={props.min || 0} max={props.max || 100} />
+                <InputSlider level={window.GlobalSettings.Quality[keys[0]][keys[1]] || 0} onChange={(newVal) => { window.GlobalSettings.Quality[keys[0]][keys[1]] = newVal }} min={props.min || 0} max={props.max || 100} scrolling={false} />
             </div>
         </div>
     )
