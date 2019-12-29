e0ef3cf5e578dd6e83ec9d4aac3b71d66455e5e2
Xander Frangos <33106561+xanderfrangos@users.noreply.github.com>
2019-12-28 22:06:58 -0500

    Removed single-file options icon

--- src/components/views/FileList.jsx
+++ src/components/views/FileList.jsx
@@ -78,12 +78,6 @@ export default class FileList extends PureComponent {
                         {makeFileItemInfoRow(file)}
                     </div>
 
-                    <div className="actions">
-                        <div className="more-button" onClick={moreMenuButton}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"></path><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path></svg>
-                        </div>
-                    </div>
-
                 </div>
             </div>)
         }
